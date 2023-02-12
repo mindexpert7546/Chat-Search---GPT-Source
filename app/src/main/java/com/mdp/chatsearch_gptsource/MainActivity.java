8e7de5991423419076fad1c91f564719276b8b03
Sagar Kumar <bashu2820159@gmail.com>
2023-02-12 18:26:07 +0530

    intent,volley library added

--- app/src/main/java/com/mdp/chatsearch_gptsource/MainActivity.java
+++ app/src/main/java/com/mdp/chatsearch_gptsource/MainActivity.java
@@ -1,15 +1,69 @@
 package com.mdp.chatsearch_gptsource;
 
+import static com.mdp.chatsearch_gptsource.R.id.chatnow;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Variable declear here
+    TextView chatnow;
+    TextView welcome;
+    TextView setting;
+    TextView share;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // hii
+
+
+
+            setContentView(R.layout.activity_main);
+
+            chatnow = findViewById(R.id.chatnow);
+            welcome= findViewById(R.id.welcome);
+            setting = findViewById(R.id.setting);
+            share = findViewById(R.id.share);
+            chatnow.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                     Intent i=new Intent(MainActivity.this,chatscreen.class);
+                     startActivity(i);
+
+
+                }
+            });
+            setting.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+
+
+                }
+            } );
+
+           share.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View view) {
+
+               }
+           }
+
+            );
+        }
+
+
+
     }
-}
--- app/src/main/java/com/mdp/chatsearch_gptsource/chatscreen.java
+++ app/src/main/java/com/mdp/chatsearch_gptsource/chatscreen.java
@@ -8,6 +8,13 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONObject;
+
 public class chatscreen extends AppCompatActivity {
 
     //Variable declear here
@@ -35,6 +42,30 @@ public class chatscreen extends AppCompatActivity {
     }
 
     private void chatLoader(){
+
+        String url = "curl https://api.openai.com/v1/models \\";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        chatview.setText("Response: " + response.toString());
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+
+                    }
+                });
+
+// Access the RequestQueue through your singleton class.
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest);
+
+
+
         // in this class volley will be implemented for get or post request
 
     }
